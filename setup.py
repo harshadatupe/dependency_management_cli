b0e0235e3ea97c58f3952340006ad95594ff32cc
Harshada Tupe <harshadatupe8@gmail.com>
2024-03-20 16:00:19 -0700

    added logic to publish pypi package

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='deps_manager',
     version='1.0',
+    author="Harshada Tupe",
+    author_email="harshadatupe8@gmail.com",
+    license="MIT",
     packages=find_packages(),
+    classifiers=["Python 3.9", "Cross Platform"],
+    python_requires='>=3.5',
     entry_points={
         'console_scripts': [
             'deps_manager = deps_manager.main:main',
