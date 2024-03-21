46689e3378b8e3861f67d0a50d06f9d2e055866c
Harshada Tupe <harshadatupe8@gmail.com>
2024-03-20 19:25:28 -0700

    added unit tests and updated package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='deps_manager',
-    version='1.0.1',
+    version='1.0.2',
     author="Harshada Tupe",
     author_email="harshadatupe8@gmail.com",
     license="MIT",
